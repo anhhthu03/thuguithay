39595395e96b8a262531e4bf2135c75754268daa
anhhthu03 <143944639+anhhthu03@users.noreply.github.com>
2023-09-03 22:46:11 +0700

    Add files via upload

---
+++ using System;.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btlythuyet { 
+    public class bt10
+    {
+        public delegate float Calc(float so1, float so2);
+
+        float Cong(float so1, float so2)
+        {
+            return (so1 + so2);
+        }
+
+        static void Main(string[] args)
+        {
+            float so1 = 5.5f;
+            float so2 = 13.5f;
+
+            bt10 b10 = new bt10();
+            Calc cl = new Calc(b10.Cong);
+            float red = cl(so1, so2);
+
+            Console.Write("{0} + {1} = {2}", so1, so2, red);
+            Console.Read();
+        }
+    }
+}
