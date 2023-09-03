4f9c3639ffaaeb00ce9ead2317c2f3c316e4afa9
anhhthu03 <143944639+anhhthu03@users.noreply.github.com>
2023-09-03 22:26:09 +0700

    Add files via upload

---
+++ using System;.cs
@@ -0,0 +1,65 @@
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+namespace btlythuyet
+
+{
+
+    class bt6_nguoi
+
+    {
+
+        // Khai báo thuộc tính
+
+        public string maso;
+
+        public string hoten;
+
+        public string gioitinh;
+
+        public bt6_nguoi(string maso, string hoten, string gioitinh)
+
+        {
+
+            this.maso = maso;
+
+            this.hoten = hoten;
+
+            this.gioitinh = gioitinh;
+
+        }
+
+        public virtual void Nhap()
+
+        {
+
+            Console.Write("Nhap ma so: ");
+
+            maso = Console.ReadLine();
+
+            Console.Write("Nhap ho ten: ");
+
+            hoten = Console.ReadLine();
+
+            Console.Write("Nhap gioi tinh: ");
+
+            gioitinh = Console.ReadLine();
+
+        }
+
+        public virtual void HienThi()
+
+        {
+
+            Console.WriteLine("Ma so: {0}, Ho ten: {1}, Gioi tinh: {2}", maso, hoten, gioitinh);
+
+        }
+
+    }
+
+}
