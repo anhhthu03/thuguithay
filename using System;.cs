c426f2c90d504bb458635a58c8042a9afad40a52
anhhthu03 <143944639+anhhthu03@users.noreply.github.com>
2023-09-03 21:21:37 +0700

    Add files via upload

---
+++ using System;.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLyThuyet
+{
+    internal class Bai3_Continue_Example
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Continue statement example");
+            for (int idx = 1; idx <= 10; idx++)
+            {
+                if (idx % 2 == 0)
+                {
+                    continue;
+                }
+                Console.Write(idx + " ");
+            }
+            Console.Read();
+        }
+            
+        }
+    }
