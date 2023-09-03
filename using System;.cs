85e64b66a15526cb68cb278ae008b14f3db1d234
anhhthu03 <143944639+anhhthu03@users.noreply.github.com>
2023-09-03 21:34:43 +0700

    Add files via upload

---
+++ using System;.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLyThuyet
+{
+    internal class Bai3foreach
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Foreach statement example");
+            int[] intArr = new int[10];
+            Random r = new Random();
+            for (int idx = 0; idx < 10; idx++)
+            {
+                intArr[idx] = r.Next(1, 10);
+            }
+            Console.Write("Value of element: ");
+            foreach (int val in intArr)
+            {
+                Console.Write(val + " ");
+            }
+            Console.Read();
+        }
+    }
+}
