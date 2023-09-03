027eab559c341c139b490b74cb162f1384e47b28
anhhthu03 <143944639+anhhthu03@users.noreply.github.com>
2023-09-03 21:06:09 +0700

    Update bt3Break_EX.cs

--- bt3Break_EX.cs
+++ bt3Break_EX.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace BTLyThuyet
 {
     internal class Bai3_Break_Example
@@ -13,12 +12,10 @@ namespace BTLyThuyet
             Console.WriteLine("Foreach statement example");
             int[] intArr = new int[10];
             Random r = new Random();
-            
             for (int idx = 0; idx < 10; idx++)
             {
                 intArr[idx] = r.Next(1, 10);
             }
-            
             Console.Write("Value of element: ");
             foreach (int val in intArr)
             {
